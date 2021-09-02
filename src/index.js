1b80056320bd011a0b80971685ec202f1c605e9b
Hugo Pacini <hugodpn@gmail.com>
2021-09-02 20:38:46 -0300

    Creo GET productos emulado

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.info("Aplicacion en NODE");
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+const productos = [
+    { id: 1, nombre: "Nike", talle: 34, precio: 15000},
+    { id: 2, nombre: "Adidas", talle: 35, precio: 25000}
+];
+
+const getProductos = async () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(productos), 1500);
+    });
+}
+
+app.get("/productos", async (req, res) => {
+
+    const productos = await getProductos();
+    res.send(productos);
+});
+
+app.listen(PORT, () => {
+    console.info(`Iniciando servidor en puerto ${PORT}`);
+});
