04b6cb60218e1c41f7541fc443613e18bd1f5ba0
Hugo Pacini <hugodpn@gmail.com>
2021-09-23 19:34:47 -0300

    Inicio de proyecto backend

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
 const PORT = 4000;
+
+app.use(cors());
 
 const productos = [
     { id: 1, nombre: "Nike", talle: 34, precio: 15000},
-    { id: 2, nombre: "Adidas", talle: 35, precio: 25000}
+    { id: 2, nombre: "Adidas", talle: 35, precio: 25000},
+    { id: 3, nombre: "xyz", talle: 35, precio: 25000}
 ];
 
 const getProductos = async () => {
@@ -14,7 +19,6 @@ const getProductos = async () => {
 }
 
 app.get("/productos", async (req, res) => {
-
     const productos = await getProductos();
     res.send(productos);
 });
